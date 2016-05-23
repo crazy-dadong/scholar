8237e148b62ace36af1c5e9da961c717398a15da
crazy_dadong@outlook.com <crazy_dadong@outlook.com>
2016-05-23 22:17:35 +0800

    我就不信了 忽略掉.vagrant

---
+++ app/Http/Controllers/User/Settings/IndexController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\User\Settings;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Intervention\Image\Facades\Image;
+
+class IndexController extends Controller
+{
+    /**
+     * 用户基本设置界面
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function getIndex(Request $request)
+    {
+        return view('user.settings.index');
+
+    }
+    
+
+    public function getImg()
+    {
+//        $data = \Input::all();
+//
+//        $img = Image::make(public_path('avatar/avatar.png'));
+//        $img->crop($data['width'], $data['height'], $data['x'], $data['y']);
+//        $img->resize(120,120);
+//        $img->save(public_path('avatar/newAvatar.png'));
+
+        Carbon::now();
+
+        $img = Image::canvas(120, 120, '#FFFFFF');
+
+        $img->text("你", 60, 60, function($font) {
+            $font->file(public_path('fonts/zhankuhei.ttf'));
+            $font->size(64);
+            $font->color('#0073b7');
+            $font->align('center');
+//            $font->valign('center');
+        });
+        $img->save(public_path('avatar/demoAvatar.png'));
+
+    }
+
+}
